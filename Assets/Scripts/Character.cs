961a053835e7c0b0d709789049f7c28067ca0abb
Mateja Stankovic <stankovicmateja@yahoo.com>
2017-07-29 14:35:21 +0200

    Movement, Shooting started

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public int speed = 5;
+    public int forceSlow = 7;
+    public float energy = 100;
+    public float bulletSpeed = 5000;
+    public GameObject gun;
+    public GameObject bullet;
+
+    private Vector3 mousePos;
+    private Vector3 objectPos;
+    private float angle;
+    private GameObject bulletInstance;
+
+
+	// Use this for initialization
+	void Start ()
+    {       
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+        RotateTowardsMouse();
+        Movement();
+    }
+
+    private void Shoot()
+    {
+        mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        bulletInstance = Instantiate(bullet);
+        bulletInstance.transform.position = new Vector2(gun.transform.position.x, gun.transform.position.y);
+        bulletInstance.GetComponent<Rigidbody2D>().AddForce(mousePos * bulletSpeed);
+        Debug.Log(mousePos * bulletSpeed);
+    }
+
+    private void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //transform.Translate(new Vector2(0, 1)*(Time.deltaTime * speed));
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * speed / forceSlow);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //transform.Translate(new Vector2(-1, 0) * (Time.deltaTime * speed));
+            GetComponent<Rigidbody2D>().AddForce(Vector2.left * speed / forceSlow);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //transform.Translate(new Vector2(0, -1) * (Time.deltaTime * speed));
+            GetComponent<Rigidbody2D>().AddForce(-Vector2.up * speed / forceSlow);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            //transform.Translate(new Vector2(1, 0) * (Time.deltaTime * speed));
+            GetComponent<Rigidbody2D>().AddForce(-Vector2.left * speed / forceSlow);
+        }
+    }
+
+    private void RotateTowardsMouse()
+    {
+        mousePos = Input.mousePosition;
+        mousePos.z = 5;
+        objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+        angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
---
+++ Assets/Scripts/DynamicCamera.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicCamera : MonoBehaviour {
+
+    /*
+    public Transform player;
+
+    private float damping = 12.0f;
+    private float height = 13.0f;
+    private float offset = 0.0f;
+    private float viewDistance = 5.0f;
+    private Vector3 center;
+    private Vector3 mousePos;
+    private Vector3 cursorPos;
+    private Vector3 playerPos;
+
+    void Update()
+        {
+            mousePos = Input.mousePosition;
+            mousePos.z = viewDistance;
+            cursorPos  = Camera.main.ScreenToWorldPoint(mousePos);
+
+            playerPos = player.position;
+
+            center = new  Vector3((playerPos.x + cursorPos.x) / 2, playerPos.y, (playerPos.z + cursorPos.z) / 2);
+
+            transform.position = Vector3.Lerp(transform.position, center + new Vector3(0, height, offset), Time.deltaTime * damping);
+    }
+    */
+    public Transform Player;
+
+    private float Damping = 12.0f;
+    private float Height = 13.0f;
+    private float Offset = 0.0f;
+    private float ViewDistance = 5.0f;
+    private Vector3 Center;
+
+ 
+     void Update()
+        {
+            var mousePos = Input.mousePosition;
+            mousePos.z = ViewDistance;
+            Vector3 CursorPosition = Camera.main.ScreenToWorldPoint(mousePos); 
+            Vector3 PlayerPosition = Player.position;
+
+            Center =new  Vector3((PlayerPosition.x + CursorPosition.x) / 2, PlayerPosition.y, (PlayerPosition.z + CursorPosition.z) / 2);
+
+            transform.position = Vector3.Lerp(transform.position, Center +new  Vector3(0, Height, Offset), Time.deltaTime * Damping);
+        }
+}
